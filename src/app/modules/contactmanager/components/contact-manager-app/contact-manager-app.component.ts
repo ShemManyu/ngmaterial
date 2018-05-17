ed33c42600f9c49928fa7d308091d944ae870d19
ShemManyu <smanyu48@gmail.com>
2018-05-17 22:11:44 +0300

    Added SVG icons

--- src/app/modules/contactmanager/components/contact-manager-app/contact-manager-app.component.ts
+++ src/app/modules/contactmanager/components/contact-manager-app/contact-manager-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-manager-app',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactManagerAppComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconSet(sanitizer.bypassSecurityTrustResourceUrl('assets/avatars.svg'));
+  }
 
   ngOnInit() {
   }
