01e0b2aff0cec420472b63a8b8043d2e83e76a48
ShemManyu <smanyu48@gmail.com>
2018-05-18 11:02:21 +0300

    Added notes table

--- src/app/modules/contactmanager/components/notes/notes.component.ts
+++ src/app/modules/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Note } from '../../models/note';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-notes',
@@ -9,9 +10,13 @@ import { Note } from '../../models/note';
 export class NotesComponent implements OnInit {
 
   @Input() notes: Note[];
-  constructor() { }
+
+  displayedColumns = ['position', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
+  constructor() {  }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
